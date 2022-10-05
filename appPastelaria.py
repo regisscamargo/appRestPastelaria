53f462d9019a35d078e89e4fa16c44e3c1a7d9ec
Régis Camargo <regisscamargo@uniplaclages.edu.br>
2022-10-05 20:44:05 -0300

    WIP - Criando páginas

--- appPastelaria.py
+++ appPastelaria.py
@@ -5,6 +5,7 @@ from mod_funcionario.funcionario import bp_funcionario
 from mod_cliente.cliente import bp_cliente
 from mod_produto.produto import bp_produto
 from mod_index.index import bp_index
+from mod_erro.erro import bp_erro
 
 app = Flask(__name__)
 # registro das rotas do blueprint
@@ -12,6 +13,7 @@ app.register_blueprint(bp_funcionario)
 app.register_blueprint(bp_cliente)
 app.register_blueprint(bp_produto)
 app.register_blueprint(bp_index)
+app.register_blueprint(bp_erro)
 
 if __name__ == "__main__":
     """ Inicia o aplicativo WEB Flask """
